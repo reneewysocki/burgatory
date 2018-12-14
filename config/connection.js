8fbae095e01c56ff42df6659803d4ab83f213e64
Renee Wysocki <reneew311@gmail.com>
2018-12-14 12:31:12 -0600

    update

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "SmallShowCoverFeed11",
-  database: "burgers_db"
-});
+var connection;
 
-connection.connect( (err)=> {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3000,
+    user: "root",
+    password: "SmallShowCoverFeed11",
+    database: "burgers_db"
+  });
+};
+
+connection.connect((err) => {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
