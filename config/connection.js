deb972f1c59c9df665f85aa2f9c09ab531ec189e
Renee Wysocki <reneew311@gmail.com>
2018-12-17 19:25:00 -0600

    server connection fix

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection;
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "SmallShowCoverFeed11",
+  database: "cat_db"
+});
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3000,
-    user: "root",
-    password: "SmallShowCoverFeed11",
-    database: "burgers_db"
-  });
-};
-
-connection.connect((err) => {
+// Make connection.
+connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -23,5 +18,6 @@ connection.connect((err) => {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection
+// Export connection for our ORM to use.
 module.exports = connection;
+
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var port = process.env.PORT || 3030;
+var port = process.env.PORT || 800;
 
 var app = express();
 
