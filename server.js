583f733e4ceb88b354d5d6f3298347021b2841d9
Renee Wysocki <reneew311@gmail.com>
2018-12-24 09:37:34 -0600

    bug fixes

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-// Dependencies 
 var express = require("express");
-var bodyParser = require("body-parser");
 
-var port = process.env.PORT || 800;
+var PORT = process.env.PORT || 8181;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Set Handlebars./
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -20,6 +20,10 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
-app.use("/", routes);
+app.use(routes);
 
-app.listen(port);
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
